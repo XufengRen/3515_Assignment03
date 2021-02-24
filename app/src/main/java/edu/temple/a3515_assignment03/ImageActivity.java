03257289d3f73643ce6add22f844c0fa98c18996
XufengRen <tuk39898@temple.edu>
2021-02-23 21:01:06 -0500

    Initial commit

---
+++ app/src/main/java/edu/temple/a3515_assignment03/ImageActivity.java
@@ -0,0 +1,14 @@
+package edu.temple.a3515_assignment03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
